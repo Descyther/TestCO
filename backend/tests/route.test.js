c0e4039091a0246745bc21439e10d308d5eacab1
ChillinNBillin <natbradzis@gmail.com>
2022-08-16 02:08:03 +0100

    First test complete. Doing frontend in morning with /metrics test

--- backend/tests/route.test.js
+++ backend/tests/route.test.js
@@ -3,9 +3,8 @@ import app from '../app.js';
 
 describe('Time route should display time in Epoch', () => {
   test('responds to /time', async () => {
-    const res = await request(app).get('/time');
-    expect(res.header.authorization[1]).toBe('mysecrettoken');
+    const res = await request(app).get('/time').set({ Authorization: 'mysecrettoken' });
     expect(res.statusCode).toBe(200);
-    expect(res.epoch.timeEpoch).toEqual(Math.floor(+new Date() / 1000));
+    expect(typeof JSON.parse(res.text).epoch.timeEpoch).toBe('number');
   });
 });
