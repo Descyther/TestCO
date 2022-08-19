38e5b3f1b46a0608e825c2ff2df833bcb022f179
ChillinNBillin <natbradzis@gmail.com>
2022-08-20 00:31:06 +0100

    Ran out of time.
    Added:
    - server time
    - metrics
    - loading
    - authorised requests.
    
    Missed:
    - Difference between server time - client time though a quick attempt is made.
    - React Testing Library usage.
    
    Fixes
    - Backend Eslint issues

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import promMid from 'express-prometheus-middleware';
+import cors from 'cors';
 import authenticateToken from './routes/helpers/auth.js';
 import indexRouter from './routes/index.js';
 
 const app = express();
 const __dirname = path.resolve();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
